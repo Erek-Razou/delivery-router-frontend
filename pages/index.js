dc8d34478c1680e420172a8615252f1ab36cbe41
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-05-04 18:17:45 +0300

    Added charts using Chart.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import React from "react";
 import dynamic from "next/dynamic";
+import BarChart from "../components/charts/barChart";
+import LineChart from "../components/charts/lineChart";
+import Data from "@/components/data";
+
+// For Charts Doc : https://upmostly.com/tutorials/how-to-use-chart-js-with-react
 
 export default function Home() {
   const MapWithNoSSR = dynamic(() => import("../components/map"), {
     ssr: false
   });
 
+
+
   return (
       <main>
         <div id="map">
           <MapWithNoSSR />
+        </div>
+        <div id={"charts"} >
+            <BarChart />
+            <LineChart/>
         </div>
       </main>
   );
