d48d66b0f4d2cb5c93ac330db05640e202e7d5c2
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-05-04 22:08:47 +0300

    Modified LineChart data to be compatible with backend

--- components/data.js
+++ components/data.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import {useEffect, useState} from "react";
 
 
-const url = "http://localhost:8080/api/v1/employers/salaries"
+const url = "http://localhost:8080/api/v1/salaries"
 export const getData = async () => {
     try {
         const { data } = await axios.get(url);
         const modifiedData = data.map((data) => ({
-            salary: data[0],
-            title: data[1]
+            salary: data.salary,
+            title: data.title
         }));
         return modifiedData;
     } catch (err) {
